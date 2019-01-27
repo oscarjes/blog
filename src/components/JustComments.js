925dd07dd2b6991b8d26d0a1a49e6542ad038348
Oscar Jesionek <oscar@jesionek.co>
2019-01-27 16:31:35 +0700

    fix window error

--- src/components/JustComments.js
+++ src/components/JustComments.js
@@ -6,35 +6,6 @@ class JustComments extends React.Component {
     this.ref = React.createRef()
   }
   render() {
-    if (window) {
-      window.jcMyTheme = {
-        buttons: {
-          primaryColor: 'white',
-          primaryBgColor: '#551A8B',
-          primaryColorAlt: '#8899a6',
-          secondaryColor: '#595959',
-          secondaryBgColor: '#333',
-          secondaryColorAlt: '#135784',
-          disabledBgColor: '#D3D3D3',
-        },
-        text: {
-          primaryColor: '#333',
-          primaryColorAlt: '#135784',
-          secondaryColor: '#595959',
-          secondaryColorAlt: '#135784',
-        },
-        borderColor: '#dedede',
-        borderRadius: '15px',
-        backgroundColor: 'white',
-        outlineStyle: '2px dotted #595959',
-        avatar: {
-          borderRadius: '50%',
-          backgroundColor: '#D3D3D3',
-          color: 'grey',
-        },
-      }
-    }
-
     return (
       <div
         ref={this.ref}
@@ -45,6 +16,32 @@ class JustComments extends React.Component {
     )
   }
   componentDidMount() {
+    window.jcMyTheme = {
+      buttons: {
+        primaryColor: 'white',
+        primaryBgColor: '#551A8B',
+        primaryColorAlt: '#8899a6',
+        secondaryColor: '#595959',
+        secondaryBgColor: '#333',
+        secondaryColorAlt: '#135784',
+        disabledBgColor: '#D3D3D3',
+      },
+      text: {
+        primaryColor: '#333',
+        primaryColorAlt: '#135784',
+        secondaryColor: '#595959',
+        secondaryColorAlt: '#135784',
+      },
+      borderColor: '#dedede',
+      borderRadius: '15px',
+      backgroundColor: 'white',
+      outlineStyle: '2px dotted #595959',
+      avatar: {
+        borderRadius: '50%',
+        backgroundColor: '#D3D3D3',
+        color: 'grey',
+      },
+    }
     const s = document.createElement('script')
     s.src = '//just-comments.com/w.js'
     s.setAttribute('data-timestamp', +new Date())
